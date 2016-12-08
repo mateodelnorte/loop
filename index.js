8f4f4a2f9f670dfd8244fc996a88df495a34c2e5
Matt Walters <mattwalters5@gmail.com>
2016-12-08 11:03:08 -0500

    adding error codes to failures

--- index.js
+++ index.js
@@ -50,7 +50,10 @@ const commands = expandCommand(dirs, command);
 run(commands, (err, commandOutputs) => {
   console.log(commandOutputs.reduce((logMessage, commandOutput, index) => {
     const color = commandOutput.error ? 'red' : 'green';
+    const code = commandOutput.error && commandOutput.error.code !== 0 
+      ? commandOutput.error.code 
+      : null;
     const output = commandOutput.output || commandOutput.error;
-    return `${logMessage}\n${chalk[color](dirs[index])}\n${output}\n`; 
+    return `${logMessage}\n${chalk[color](dirs[index])}${code ? ` (exit code ${code})` : ''}\n${output}\n`; 
   }, ''));
 });
